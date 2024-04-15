6ba0408e088badc8647a97ae7407ef84698503a9
Nazif Dilmen <157618507+NazifD@users.noreply.github.com>
2024-04-14 22:38:49 -0400

    Update main.R

--- main.R
+++ main.R
@@ -12,3 +12,6 @@ model <- lm(price ~ bdrms, data = hprice1)
 summary_output <- summary(model)
 bedroom_coefficient <- summary_output$coefficients[2,1]
 cat(sprintf("for the average home in this dataset, we can reasonably expect that if you added an extra bedroom you would increase the expected selling price by $%.2f", bedroom_coefficient))
+
+#NAZ EDIT
+the output when ran was: "for the average home in this dataset, we can reasonably expect that if you added an extra bedroom you would increase the expected selling price by $62.02"

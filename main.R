2c0b0e7c9ab76cd22216df58ba764fbcb7afbaac
Nazif Dilmen <157618507+NazifD@users.noreply.github.com>
2024-04-14 22:37:38 -0400

    Add files via upload

---
+++ main.R
@@ -0,0 +1,14 @@
+rm=(list())
+#install.packages("wooldridge")
+library(wooldridge)
+
+# 2. Load the dataset 'hprice1' by running data('hprice1')
+data('hprice1')
+
+# 3. Create a model to estimate the effect on price of adding a bedroom.
+model <- lm(price ~ bdrms, data = hprice1)
+
+# 4. Complete this sentence "for the average home in this dataset, we can reasonably expect that if you added an extra bedroom you would increase the expected selling price by $___"
+summary_output <- summary(model)
+bedroom_coefficient <- summary_output$coefficients[2,1]
+cat(sprintf("for the average home in this dataset, we can reasonably expect that if you added an extra bedroom you would increase the expected selling price by $%.2f", bedroom_coefficient))
